1725327a15428ee24330040b0d96c737eac93083
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-11-22 18:24:18 +0200

    Refactored User and UserDetails service for multi-tenancy security

--- ssc-brewery/src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/security/JpaUserDetailsService.java
@@ -1,22 +1,13 @@
 package guru.sfg.brewery.security;
 
-import guru.sfg.brewery.domain.security.Authority;
-import guru.sfg.brewery.domain.security.User;
 import guru.sfg.brewery.repositories.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -31,28 +22,10 @@ public class JpaUserDetailsService implements UserDetailsService {
 
         log.debug(() -> "Getting user info for: " + username);
 
-        User user = userRepository.findByUsername(username).orElseThrow(() -> {
-           return new UsernameNotFoundException("Username: " + username + " not found!");}
+        return userRepository.findByUsername(username).orElseThrow(() ->
+           new UsernameNotFoundException("Username: " + username + " not found!")
            ); //You need to use transactional because
               // some properties are lazy and other are eager
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                user.getEnabled(),
-                user.getAccountNonExpired(),
-                user.getCredentialsNonExpired(),
-                user.getAccountNonLocked(),
-                convertToSpringAuthorities(user.getAuthorities()));
     }
 
-    private Collection<? extends GrantedAuthority> convertToSpringAuthorities(Set<Authority> authorities) {
-        if (authorities != null && authorities.size() > 0){
-            return authorities.stream()
-                    .map(Authority::getPermission)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        } else {
-            return new HashSet<>();
-        }
-    }
 }
