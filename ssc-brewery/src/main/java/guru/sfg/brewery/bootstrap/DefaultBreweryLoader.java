1749f0922b62aa892a975f0de321921022cbebb8
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-11-25 23:19:28 +0200

    Added secured pickup order

--- ssc-brewery/src/main/java/guru/sfg/brewery/bootstrap/DefaultBreweryLoader.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/bootstrap/DefaultBreweryLoader.java
@@ -225,10 +225,12 @@ public class DefaultBreweryLoader implements CommandLineRunner {
         Authority readOrder = authorityRepository.save(Authority.builder().permission("order.read").build());
         Authority updateOrder = authorityRepository.save(Authority.builder().permission("order.update").build());
         Authority deleteOrder = authorityRepository.save(Authority.builder().permission("order.delete").build());
+        Authority pickupOrder = authorityRepository.save(Authority.builder().permission("order.pickup").build());
         Authority createOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.create").build());
         Authority readOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.read").build());
         Authority updateOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.update").build());
         Authority deleteOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.delete").build());
+        Authority pickupOrderCustomer = authorityRepository.save(Authority.builder().permission("customer.order.pickup").build());
 
 
         Role adminRole = roleRepository.save(Role.builder().roleName("ADMIN").build());
@@ -237,11 +239,11 @@ public class DefaultBreweryLoader implements CommandLineRunner {
 
         adminRole.setAuthorities(new HashSet<>(Set.of(createBeer, updateBeer, readBeer, deleteBeer, createCustomer, readCustomer,
                 updateCustomer, deleteCustomer, createBrewery, readBrewery, updateBrewery, deleteBrewery, createOrder, readOrder,
-                updateOrder, deleteOrder)));
+                updateOrder, deleteOrder, pickupOrder)));
         // You need mutable
         // collections for hibernate to persist
         customerRole.setAuthorities(new HashSet<>(Set.of(readBeer, readCustomer, readBrewery, createOrderCustomer,
-                readOrderCustomer, updateOrderCustomer, deleteOrderCustomer)));
+                readOrderCustomer, updateOrderCustomer, deleteOrderCustomer, pickupOrderCustomer)));
 
         userRole.setAuthorities(new HashSet<>(Set.of(readBeer)));
 
