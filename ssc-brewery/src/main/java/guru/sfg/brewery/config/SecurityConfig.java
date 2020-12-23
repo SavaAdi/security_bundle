949afa66e1fd98cb9105779856be876b1af44924
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2020-12-23 23:37:08 +0200

    Enabled csrf for the app, except for h2 and the rest api endpoints

--- ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated().and()
                 .formLogin().and()
                 .httpBasic()
-                .and().csrf().disable();
+                .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**");
 
         //h2 console config
         http.headers().frameOptions().sameOrigin();
